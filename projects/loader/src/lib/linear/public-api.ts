e338019e12c9c7994af72a06cdb2502edd7b5d22
Dominik <domin5892@gmail.com>
2020-12-18 20:29:44 +0100

    fix(loader/linear): Correct exports

--- projects/loader/src/lib/linear/public-api.ts
+++ projects/loader/src/lib/linear/public-api.ts
@@ -1,1 +1,4 @@
-export * from './loader.service';
+export * from './src/loader.component';
+export * from './src/loader.models';
+export * from './src/loader.module';
+export * from './src/loader.service';
